0a12da185db891ae1d880b2cf81137ccb5cf7347
Raian de Andrades <raian.andrades@gmail.com>
2019-11-23 16:43:10 -0300

    ESLint, Prettier & EditorConfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello World2" });
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello World2' });
 });
 
 export default routes;
