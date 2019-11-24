3be15321102d8f044682a653c5443427f920c00a
Raian de Andrades <raian.andrades@gmail.com>
2019-11-23 21:10:45 -0300

    Migration de usuário

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'gobarber',
+  database: 'postgres',
   define: {
     timestamps: true,
     underscored: true,
