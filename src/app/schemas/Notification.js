fcbd8c8e0f6d5ed46f765d4f535cf081699acd68
Raian de Andrades <raian.andrades@gmail.com>
2019-12-08 20:20:21 -0300

    Notificando novos agendamentos

---
+++ src/app/schemas/Notification.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const NotificationSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Number,
+      required: true,
+      default: false,
+    },
+    read: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model('Notification', NotificationSchema);
